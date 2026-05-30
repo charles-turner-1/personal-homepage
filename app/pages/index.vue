a13bb1b6b518f133462b4de10b3299ff0f102158
Charles Turner <charles.turner@anu.edu.au>
2026-05-30 17:26:00 +0800

    Remove router from homepage index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
 
-import type { NavigationMenuItem } from '@nuxt/ui';
-
-const route = useRoute();
-
-const items = computed<NavigationMenuItem[]>(() => [
-  { label: 'Home', to: '/' , active: route.path === ('/') },
-  { label: 'About Me', to: '/aboutme', active: route.path.startsWith('/aboutme') },
-  { label: 'Projects', to: '/projects', active: route.path.startsWith('/projects') },
-  { label: 'Blog', to: '/interactive-glodap-catalog', active: route.path.startsWith('/interactive-glodap-catalog') },
-  { label: 'Contact', to: '/contact', active: route.path.startsWith('/contact') },
-]);
-
 definePageMeta({
   title: 'Home',
   layout: 'default',
